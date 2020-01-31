757aeb389732404ee4c96cef65a7710eec06ce8a
hdpuk86 <hayley.d.prior@gmail.com>
2020-01-31 11:03:51 +0000

    Checkout can have rules

--- shopping_basket_tests.rb
+++ shopping_basket_tests.rb
@@ -4,7 +4,18 @@ class ShoppingBasketTests < MiniTest::Test
   def test_can_create_checkout
     assert Checkout.new
   end
+
+  def test_checkout_can_have_rules
+    rules = []
+    co = Checkout.new(rules)
+    assert_equal rules, co.rules
+  end
 end
 
 class Checkout
+  attr_accessor :rules
+
+  def initialize(rules=[])
+    @rules = rules
+  end
 end
