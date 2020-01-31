dd908a518cbb3eec5ee4a0ec181d93f74ea635be
hdpuk86 <hayley.d.prior@gmail.com>
2020-01-31 12:17:27 +0000

    calculate basket discount

--- shopping_basket_tests.rb
+++ shopping_basket_tests.rb
@@ -2,7 +2,8 @@ require 'minitest/autorun'
 
 class ShoppingBasketTests < MiniTest::Test
   def setup
-    @rules = []
+    basket_promo = Promo.new(100, 20)
+    @rules = [basket_promo]
     @co = Checkout.new(@rules)
     @item_a = Item.new(2)
   end
@@ -36,6 +37,31 @@ class ShoppingBasketTests < MiniTest::Test
     @co.scan(@item_a)
     assert_equal @item_a.price, @co.total
   end
+
+  def test_basket_discount_can_get_applied
+    item = Item.new(100)
+    @co.scan(item)
+    assert_equal 80, @co.total
+  end
+
+  def test_basket_discount_does_not_get_applied_if_target_not_met
+    item = Item.new(99)
+    @co.scan(item)
+    assert_equal 99, @co.total
+  end
+end
+
+class Promo
+  attr_accessor :target_amount, :discount
+
+  def initialize(target_amount, discount)
+    @target_amount = target_amount
+    @discount = discount
+  end
+
+  def calculate_discount(basket)
+    return self.discount if basket.total >= self.target_amount
+  end
 end
 
 class Item
@@ -46,15 +72,32 @@ class Item
   end
 end
 
+class Basket
+  attr_accessor :total
+
+  def initialize
+    @total = 0
+  end
+end
+
 class Checkout
-  attr_accessor :rules, :total
+  attr_accessor :rules, :basket
 
   def initialize(rules=[])
     @rules = rules
-    @total = 0
+    @basket = Basket.new
+  end
+
+  def total
+    self.basket.total - discounts
   end
 
   def scan(item)
-    self.total += item.price
+    self.basket.total += item.price
+  end
+
+  def discounts
+    all_promo_discounts = self.rules.map { |rule| rule.calculate_discount(self.basket) }
+    all_promo_discounts.compact.sum
   end
 end
