416609f596dab911f251917fcc398d100252c008
hdpuk86 <hayley.d.prior@gmail.com>
2020-01-31 14:31:25 +0000

    refactor calculation into smaller functions

--- rule_types/multibuy_promo.rb
+++ rule_types/multibuy_promo.rb
@@ -8,13 +8,26 @@ class MultibuyPromo
   end
 
   def calculate_discount(basket)
-    correct_items = basket.items.filter { |basket_item| basket_item == self.item }
-    number_of_correct_items = correct_items.length
-    number_of_extra_items = number_of_correct_items % self.target_item_number
-    extra_items_cost = number_of_extra_items * self.item.price
+    promo_cost = ((number_of_correct_items(basket) - number_of_extra_items(basket))/ self.target_item_number) * self.promo_price
+    new_total = promo_cost + cost_of_extra_items(basket)
+    (number_of_correct_items(basket) * self.item.price) - new_total
+  end
 
-    promo_cost = ((number_of_correct_items - number_of_extra_items)/ self.target_item_number) * self.promo_price
-    new_total = promo_cost + extra_items_cost
-    (number_of_correct_items * self.item.price) - new_total
+  private
+
+  def correct_items(basket)
+    basket.items.filter { |basket_item| basket_item == self.item }
+  end
+
+  def number_of_correct_items(basket)
+    correct_items(basket).length
+  end
+
+  def number_of_extra_items(basket)
+    number_of_correct_items(basket) % self.target_item_number
+  end
+
+  def cost_of_extra_items(basket)
+    number_of_extra_items(basket) * self.item.price
   end
 end
