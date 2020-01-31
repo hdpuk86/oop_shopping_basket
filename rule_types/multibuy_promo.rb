87b425b8d081e02d83646c36bed5f7186add558f
hdpuk86 <hayley.d.prior@gmail.com>
2020-01-31 15:11:24 +0000

    refactor calculation for single responsiblity

--- rule_types/multibuy_promo.rb
+++ rule_types/multibuy_promo.rb
@@ -1,33 +1,47 @@
 class MultibuyPromo
-  attr_accessor :promo_price, :item, :target_item_number
+  attr_accessor :promo_price, :promo_item, :target_number_of_items
 
-  def initialize(item, target_item_number, promo_price)
-    @item = item
-    @target_item_number = target_item_number
+  def initialize(promo_item, target_number_of_items, promo_price)
+    @promo_item = promo_item
+    @target_number_of_items = target_number_of_items
     @promo_price = promo_price
   end
 
   def calculate_discount(basket)
-    promo_cost = ((number_of_correct_items(basket) - number_of_extra_items(basket))/ self.target_item_number) * self.promo_price
-    new_total = promo_cost + cost_of_extra_items(basket)
-    (number_of_correct_items(basket) * self.item.price) - new_total
+    basket.total - total_after_discount(basket)
   end
 
   private
 
-  def correct_items(basket)
-    basket.items.filter { |basket_item| basket_item == self.item }
+  def total_after_discount(basket)
+    total_cost_of_valid_promo_items(basket) + total_cost_of_exess_promo_items(basket)
   end
 
-  def number_of_correct_items(basket)
-    correct_items(basket).length
+  def total_cost_of_valid_promo_items(basket)
+    self.promo_price * times_promotion_should_be_applied(basket)
   end
 
-  def number_of_extra_items(basket)
-    number_of_correct_items(basket) % self.target_item_number
+  def times_promotion_should_be_applied(basket)
+    number_of_valid_promo_items(basket) / self.target_number_of_items
   end
 
-  def cost_of_extra_items(basket)
-    number_of_extra_items(basket) * self.item.price
+  def number_of_valid_promo_items(basket)
+    total_number_of_promo_items(basket) - excess_promo_items(basket)
+  end
+
+  def all_promo_items(basket)
+    basket.items.filter { |basket_item| basket_item == self.promo_item }
+  end
+
+  def total_number_of_promo_items(basket)
+    all_promo_items(basket).length
+  end
+
+  def excess_promo_items(basket)
+    total_number_of_promo_items(basket) % self.target_number_of_items
+  end
+
+  def total_cost_of_exess_promo_items(basket)
+    excess_promo_items(basket) * self.promo_item.price
   end
 end
